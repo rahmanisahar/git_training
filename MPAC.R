b2808a4aad47b5a13e11de3030a0c75a159b8beb
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2016-09-15 12:06:30 -0400

    Added the validation section

--- MPAC.R
+++ MPAC.R
@@ -8,5 +8,12 @@ data<-read.csv("validate-vars.txt", colClasses=column_classes)
 # Frequency counts
 # Compare means
 # Cross tabs
+
+
 # Validate variables
+# only print rows that have a valid variable in "Var": A,B,C,D (not E)
+# http://stackoverflow.com/questions/1686569/filtering-a-data-frame-in-r
+validdata<-data[data$Var %in% c("A","B","C","D"),]
+
+
 # Summing by ID
