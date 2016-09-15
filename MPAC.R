4f262b1d43e731dd0015e45666095775662f842b
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2016-09-15 10:09:11 -0400

    This is a terrible change. I should not have changed this.

--- MPAC.R
+++ MPAC.R
@@ -1,8 +1,8 @@
-# Read in the (fake) 2020.csv file
+# Read in thklmd'asv'flamvoi;namvem'val'e (fake) 2020.csv file
 # Read in a text file and make sure that the first column is character
 # http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
-column_classes=c("character","numeric","character")
-data<-read.csv("validate-vars.txt", colClasses=column_classes)
+colusdvajia'mafe'lvad mn_classes=c("character","numeafe'o'aewm'gwearic","character")
+data<-read.csv("afjp'efwaijo[aegoiegvajwm'avvalidate-vars.txt", colClasses=column_classes)
 
 
 # Frequency counts
